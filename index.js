4668fbc17a4d18e08028e3cc129a1691e574abe7
hudahamid <huda.00091581@gmail.com>
2023-01-09 10:32:53 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", () =>
+//   console.log("The DOM has Loaded")
+// );
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let paragraph = document.querySelector("p");
+    paragraph .textContent = "This is really cool!";
+  });
